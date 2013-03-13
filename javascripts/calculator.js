533e41ac67a3fb7ebd2d1910dd6477f2091793fb
Subasy <sylvie917@gmail.com>
2013-03-13 20:51:51 +0100

    Issue #4: Optimize the evaluation of functions

--- javascripts/calculator.js
+++ javascripts/calculator.js
@@ -1,193 +1,108 @@
 var canvas;
 var context; 
 
-canvas = document.getElementById('myCanvas');//Get the canvas
-context = canvas.getContext('2d'); //had a style for the canvas
-drawAxes(); // Draw the axes.
+canvas = document.getElementById('myCanvas');
+context = canvas.getContext('2d'); 
+drawAxes(); 
 
 //Function : calculate axes dimensions.
 function showAxes(ctx,axes) {
 
-	var x0=axes.x0, w=ctx.canvas.width;
-	var y0=axes.y0, h=ctx.canvas.height;
-	var xmin = axes.doNegativeX ? 0 : x0;
-	
-	ctx.beginPath();
-	ctx.strokeStyle = "rgb(128,128,128)"; 
-	ctx.moveTo(xmin,y0); ctx.lineTo(w,y0);  // X axis
-	ctx.moveTo(x0,0);    ctx.lineTo(x0,h);  // Y axis
-	ctx.stroke();
+  var x0=axes.x0, w=ctx.canvas.width;
+  var y0=axes.y0, h=ctx.canvas.height;
+  var xmin = axes.doNegativeX ? 0 : x0;
+
+  ctx.beginPath();
+  ctx.strokeStyle = "rgb(128,128,128)"; 
+  ctx.moveTo(xmin,y0); ctx.lineTo(w,y0);  
+  ctx.moveTo(x0,0);    ctx.lineTo(x0,h);  
+  ctx.stroke();
 	
 }
 
 //Function: draw axes.
 function drawAxes(){
 
-	var axes={};//, ctx=canvas.getContext("2d");
-	canvas.width=window.innerWidth-window.innerWidth/5;
-	canvas.height=window.innerHeight-window.innerHeight/3;
-	axes.x0 = .5 + .5*canvas.width;  // x0 pixels from left to x=0
-	axes.y0 = .5 + .5*canvas.height; // y0 pixels from top to y=0
-	axes.scale = 10;                 // 40 pixels from x=0 to x=1
-	axes.doNegativeX = true;
-	showAxes(context,axes);
-	canvas.setAttribute("style", "position: absolute; x:0; y:0;");
-	context.translate(canvas.width / 2, canvas.height / 2);
+  var axes={};
+  canvas.width=window.innerWidth-window.innerWidth/5;
+  canvas.height=window.innerHeight-window.innerHeight/3;
+  axes.x0 = .5 + .5*canvas.width;  
+  axes.y0 = .5 + .5*canvas.height; 
+  axes.scale = 10;                 
+  axes.doNegativeX = true;
+  showAxes(context,axes);
+  canvas.setAttribute("style", "position: absolute; x:0; y:0;");
+  context.translate(canvas.width / 2, canvas.height / 2);
 }
 
 //Function : generate random colors.
 function get_random_color() {
-	var letters = '0123456789ABCDEF'.split('');
-	var color = '#';
-	for (var i = 0; i < 6; i++ ) {
-		color += letters[Math.round(Math.random() * 10)];
-	}
-	return color;
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.round(Math.random() * 10)];
+  }
+  return color;
 }
 
 //Function : calculate the mathematic expression and print it.
 function calculate(){
 
-	var buffer1=new ArrayBuffer(1000*4); //buffer for x values
-	var buffer2=new ArrayBuffer(1000*4); // buffer for y values
-	var valeurs_x=new Int32Array(buffer1); 	//typed array for x values
-	var valeurs_y=new Int32Array(buffer2); 	//typed array for y values
-	var j=0; // an iterator
-	var x; // x value variable
-	var y; // y value variable
-	var expressionY = document.getElementById("expY").value;//Get the formula entered the user for F(Y)
-	var expressionX= document.getElementById("expX").value; //Get the formule entered by the user for F(X)
-	var ok=true;
-	var replacement = new Function("expression","value",
-		"var sin=\"Math.sin\";\n"+
-		"var cos=\"Math.cos\";\n"+
-		"var abs=\"Math.abs\";\n"+
-		"var ceil=\"Math.ceil\";\n"+
-		"var floor=\"Math.floor\";\n"+
-		"var round=\"Math.round\";\n"+
-		"var sqrt=\"Math.sqrt\";\n"+
-		"var exp=\"Math.exp\";\n"+
-		"var log=\"Math.log\";\n"+
-		"var tan=\"Math.tan\";\n"+
+  var buffer1=new ArrayBuffer(1000*4);
+  var buffer2=new ArrayBuffer(1000*4); 
+  var valeurs_x=new Float32Array(buffer1); 	
+  var valeurs_y=new Float32Array(buffer2); 	
+  var j=0; 
+  var x;
+  var y; 
+  var expressionY = document.getElementById("expY").value;
+  var minX = eval(document.getElementById("minX").value);
+  var maxX = eval(document.getElementById("maxX").value);
+  var minY = eval(document.getElementById("minY").value);
+  var maxY = eval(document.getElementById("maxY").value);
+ 
+  //evaluate the expression entered by the user with the x value.
+  var expression = eval("(function(x) {"+
+    "var sin=Math.sin;"+
+    "var cos=Math.cos;"+
+	"var abs=Math.abs;"+
+	"var ceil=Math.ceil;"+
+	"var floor=Math.floor;"+
+	"var round=Math.round;"+
+	"var sqrt=Math.sqrt;"+
+	"var exp=Math.exp;"+
+	"var log=Math.log;"+
+	"var tan=Math.tan;"+
+	"var x=x;"+
+	"return "+expressionY+";})");
 	
-		"if(expression.contains(\"sin\")){\n"+
-			"expression=expression.replace(\"sin\", sin);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"cos\")){\n"+
-			"expression=expression.replace(\"cos\", cos);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"abs\")){\n"+
-			"expression=expression.replace(\"abs\", abs);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"ceil\")){\n"+
-			"expression=expression.replace(\"ceil\", ceil);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"floor\")){\n"+
-			"expression=expression.replace(\"floor\", floor);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"round\")){\n"+
-			"expression=expression.replace(\"round\", round);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"sqrt\")){\n"+
-			"expression=expression.replace(\"sqrt\", sqrt);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"exp\")){\n"+
-			"expression=expression.replace(\"exp\", exp);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"log\")){\n"+
-			"expression=expression.replace(\"log\", log);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"tan\")){\n"+
-			"expression=expression.replace(\"tan\", tan);\n"+
-		"}\n"+
-		
-		"if(expression.contains(\"x\")){\n"+
-		"x=value;\n"+
-		"}"+
-		"if(expression.contains(\"y\")){\n"+
-		"y=value;\n"+
-		"}"+
-		"return eval(expression);\n");
-		
-	drawAxes(); // draw the axes.
-	
-	if(expressionY != "" && expressionX != ""){
-		alert("You must enter only one expression ! ");
-		ok =false;
+  drawAxes(); 
+
+  //get only the values of the domain indicated by the user.
+  for(i=minX; i<=maxX; i=i+0.1){
+    y=expression(i);
+	if(y>=minY && y<=maxY){
+	  valeurs_x[j]=i;
+	  valeurs_y[j]=y;
+	  j++;
 	}
-		
-	if(expressionY.contains("y")){
-		alert("You must only enter an expression containing x");
-		expressionY="";
-	}
+  }
 
-	if(expressionX.contains("x")){
-		alert("You must only enter an expression containing y");
-		expressionX="";
-	}
-	
-	if(expressionY != "" && ok){
-		//for 20000 values of x.
-		for(i=-500;i<=500;i++){
+  context.beginPath(); 
 
-			valeurs_x[j]=i; //add x to the x .
-			y=replacement(expressionY,i); //evaluate the formula.
-			valeurs_y[j]=y; //Add the evaluated value of y in y array.
-			j++;
-		}
+  j=0; 
 
-		context.beginPath(); // init the drawing path
-
-		var j=0; //a variable to lookup the arrays values.
-
-		//While the array has values.
-		while(j<1000){
-
-		//Draw the liaison beetween x and y values.
-		context.moveTo(10*valeurs_x[j],10*(valeurs_y[j])* -1);
-		context.lineTo(10*valeurs_x[j+1],10*(valeurs_y[j+1]) * -1);
-		j++;
-
-		}
-	}
-	
-	if(expressionX != "" && ok){
-		//for 20000 values of x.
-		for(i=-500;i<=500;i++){
-			valeurs_y[j]=i; //add x to the x .
-			x=replacement(expressionX,i); //evaluate the formula.
-			valeurs_x[j]=x; //Add the evaluated value of y in y array.
-			j++;
-		}
-		
-		context.beginPath(); // init the drawing path
-		
-		j=0; //a variable to lookup the arrays values.
-
-		//While the array has values.
-		while(j<1000){
-			//Draw the liaison beetween x and y values.
-			context.moveTo(10*valeurs_x[j],10*(valeurs_y[j])* -1);
-			context.lineTo(10*valeurs_x[j+1],10*(valeurs_y[j+1]) * -1);
-			j++;	
-		}
-	
-	}
-	
-	context.lineWidth = 2; //line growth
-	context.strokeStyle = get_random_color(); // line color
-	context.stroke(); // draw the lines
-	
-	
+  //Draw the graph
+  while(j < valeurs_x.length){
+	context.moveTo(10*valeurs_x[j],10*(valeurs_y[j]) * -1);
+	context.lineTo(10*valeurs_x[j+1],10*(valeurs_y[j+1]) * -1);	
+	j=j+1;
+  }
+  
+  context.lineWidth = 2; 
+  context.strokeStyle = get_random_color(); 
+  context.stroke(); 	
 }
+
 
 
