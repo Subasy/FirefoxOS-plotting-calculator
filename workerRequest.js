ae4a3d1f8f79890ffd6356beb7490cb4f7186f8e
Subasy <sylvie917@gmail.com>
2013-03-26 16:55:25 +0100

    issue #5 : File which contains worker requests

---
+++ workerRequest.js
@@ -0,0 +1,14 @@
+var WORKER_MANAGE_REQUEST = {
+"START" : 0,
+"SET_DATA1" : 1,
+"PERFORM" : 2,
+"CLOSE" : 3
+/* ... */
+};
+
+var WORKER_MESSAGE = {
+"CMD1" : 0,
+"CMD2" : 1,
+"LOADED" : 2
+/* ... */
+};
