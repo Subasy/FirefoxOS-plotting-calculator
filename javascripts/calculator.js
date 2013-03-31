2fd84481d46cd5b051ef9c75229c0d92f490db58
Subasy <sylvie917@gmail.com>
2013-03-31 18:11:47 +0300

    test de l'api web activities
    Ajout de l'option pick.

--- javascripts/calculator.js
+++ javascripts/calculator.js
@@ -102,6 +102,26 @@ function calculate(){
   context.stroke(); 	
 }
 
+var pickImage = document.querySelector("#pick-image");
+    if (pickImage) { 
+        pickImage.onclick = function () {
+             var pick = new MozActivity({
+                 name: "pick",
+                 data: {
+                     type: ["image/png", "image/jpg", "image/jpeg"]
+                  }
+             });
 
+            pick.onsuccess = function () { 
+                var img = document.createElement("img");
+                img.src = window.URL.createObjectURL(this.result.blob);
+                var imagePresenter = document.querySelector("#image-presenter");
+                imagePresenter.appendChild(img);
+                imagePresenter.style.display = "block";
+            };
 
-
+            pick.onerror = function () { 
+                alert("Can't view the image!");
+            };
+        }
+    }
