60dc38b22349707fc115710c9b904424cb1365d4
Subasy <sylvie917@gmail.com>
2013-04-01 15:11:32 +0300

    Adding comments.

--- javascripts/calculator.js
+++ javascripts/calculator.js
@@ -5,7 +5,10 @@ canvas = document.getElementById('myCanvas');
 context = canvas.getContext('2d'); 
 drawAxes(); 
 
-//Function : calculate axes dimensions.
+  /***************************************************************
+     Function showAxes(ctx,axes)
+	 enables to calculate axes for the graph.
+  ***************************************************************/
 function showAxes(ctx,axes) {
 
   var x0=axes.x0, w=ctx.canvas.width;
@@ -20,7 +23,10 @@ function showAxes(ctx,axes) {
 	
 }
 
-//Function: draw axes.
+  /***************************************************************
+     Function drawAxes()
+	 enables to draw axes for the graph.
+  ***************************************************************/
 function drawAxes(){
 
   var axes={};
@@ -35,7 +41,10 @@ function drawAxes(){
   context.translate(canvas.width / 2, canvas.height / 2);
 }
 
-//Function : generate random colors.
+  /***************************************************************
+     Function get_random_color()
+	 enables to generate random color for each new graph.
+  ***************************************************************/
 function get_random_color() {
   var letters = '0123456789ABCDEF'.split('');
   var color = '#';
@@ -45,80 +54,88 @@ function get_random_color() {
   return color;
 }
 
-//Function : calculate the mathematic expression and print it.
+  /***************************************************************
+     Function calculate()
+	 enables to print the graph.
+  ***************************************************************/
 function calculate(){
 
   var buffer1=new ArrayBuffer(1000*4);
   var buffer2=new ArrayBuffer(1000*4); 
   var valeurs_x=new Float32Array(buffer1); 	
   var valeurs_y=new Float32Array(buffer2); 	
-  var j=0; 
-  var x;
-  var y; 
   var expressionY = document.getElementById("expY").value;
-  var minX = parseFloat(document.getElementById("minX").value);
-  var maxX = parseFloat(document.getElementById("maxX").value);
-  var minY = parseFloat(document.getElementById("minY").value);
-  var maxY = parseFloat(document.getElementById("maxY").value);
- 
-  //evaluate the expression entered by the user with the x value.
-  var expression = eval("(function(x) {"+
-    "var ln=Math.ln;"+
-    "var sin=Math.sin;"+
-    "var cos=Math.cos;"+
-    "var abs=Math.abs;"+
-    "var ceil=Math.ceil;"+
-    "var floor=Math.floor;"+
-    "var round=Math.round;"+
-    "var sqrt=Math.sqrt;"+
-    "var exp=Math.exp;"+
-    "var log=Math.log;"+
-    "var tan=Math.tan;"+
-    "var x=x;"+
-    "return "+expressionY+";})");
-	
-  drawAxes(); 
+  
+  /*verify if all the fields are filled.*/
+  if(expressionY == "" || 
+  document.getElementById("minX").value == "" || 
+  document.getElementById("maxX").value=="" || 
+  document.getElementById("minY").value=="" || 
+  document.getElementById("maxY").value== ""){
+  alert("Please fill all the field !");
+  }
+  
+  else{
+  /* If values are filled :
+	 1/Recover the values.
+	 2/Create the worker.
+	 3/Recover the arrays filled by the worker.
+	 @see "myWorker.js"  */
+	 
+    var minX = parseFloat(document.getElementById("minX").value);
+    var maxX = parseFloat(document.getElementById("maxX").value);
+    var minY = parseFloat(document.getElementById("minY").value);
+    var maxY = parseFloat(document.getElementById("maxY").value);
 
-  //get only the values of the domain indicated by the user.
-  for(x=minX; x<=maxX; x=x+0.1){
-    y=expression(x);
-	if(y>=minY && y<=maxY){
-	  valeurs_x[j]=x;
-	  valeurs_y[j]=y;
-	  j++;
+	drawAxes(); 
+	  
+    this.worker = new Worker('myWorker.js');
+    this.worker.addEventListener('message', function(e) {
+    var data = e.data;
+	if(data.cmd!=undefined){
+	  switch(data.cmd){
+	  case WORKER_MESSAGE.CMD1:
+	    this.valeurs_x=data.valeurs_x;
+		this.valeurs_y=data.valeurs_y;
+		
+  /* 1/The graph is drawn thanks to the creation of coordinates 
+	 with the arrays valeurs_x and valeurs_y values.
+	 2/Line Width is about 2 pixel.
+	 3/A random color is generated for each new graph. */
+	 
+		context.beginPath(); 
+			
+		for(j=0;j<valeurs_x.length;j++){
+		  context.moveTo(10*this.valeurs_x[j],10*(this.valeurs_y[j]) * -1);
+		  context.lineTo(10*this.valeurs_x[j+1],10*(this.valeurs_y[j+1]) * -1);	
+		}
+
+		context.lineWidth = 2; 
+		context.strokeStyle = get_random_color(); 
+		context.stroke(); 
+		break;
+	  }
 	}
-  }
+	else{
+		console.log(data);
+	}
+  }, false);
 
-  context.beginPath(); 
-  
-  //Draw the graph
-  for(j=0;j<valeurs_x.length;j++){
-	context.moveTo(10*valeurs_x[j],10*(valeurs_y[j]) * -1);
-	context.lineTo(10*valeurs_x[j+1],10*(valeurs_y[j+1]) * -1);	
+  /* 1/ Initiate the worker
+     2/ send the data
+	 3/ do action in the data
+	 4/ close the worker
+	 @see "workerRequest.js" */
+	 
+  this.worker.postMessage({'cmd': WORKER_MANAGE_REQUEST.START});
+  this.worker.postMessage({'cmd': WORKER_MANAGE_REQUEST.SET_DATA1,
+  'expressionY' : expressionY,
+  'minX' : minX,
+  'maxX' : maxX,
+  'minY' : minY,
+  'maxY' : maxY
+  });
+  this.worker.postMessage({'cmd': WORKER_MANAGE_REQUEST.PERFORM});
+  this.worker.postMessage({'cmd': WORKER_MANAGE_REQUEST.CLOSE});
   }
-  
-  context.lineWidth = 2; 
-  context.strokeStyle = get_random_color(); 
-  context.stroke(); 	
 }
- 
-var pickAnything = document.querySelector("#pick-anything");
-    if (pickAnything) { 
-        pickAnything.onclick = function () {
-             var pickAny = new MozActivity({
-                 name: "pick"
-             });
-
-            pickAny.onsuccess = function () {
-                var img = document.createElement("img");
-                if (this.result.blob.type.indexOf("image") != -1) {
-                    img.src = window.URL.createObjectURL(this.result.blob);
-                    document.querySelector("#image-presenter").appendChild(img);
-                }
-            };
-
-            pickAny.onerror = function () {
-                console.log("An error occurred");
-            };
-        }
-    }
