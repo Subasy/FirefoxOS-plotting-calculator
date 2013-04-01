31738149f9e062ba2a8f1baa23e416019b4c54f2
Subasy <sylvie917@gmail.com>
2013-04-01 15:12:44 +0300

    Adding comments.

--- javascripts/workerRequest.js
+++ javascripts/workerRequest.js
@@ -3,12 +3,12 @@ var WORKER_MANAGE_REQUEST = {
 "SET_DATA1" : 1,
 "PERFORM" : 2,
 "CLOSE" : 3
-/* ... */
+/* START : Initiate the worker
+   SET_DATA1 : Get teh datas
+   PERFORM : do the treatement with the datas
+   CLOSE : close the worker */
 };
 
 var WORKER_MESSAGE = {
-"CMD1" : 0,
-"CMD2" : 1,
-"LOADED" : 2
-/* ... */
+"CMD1" : 0
 };
