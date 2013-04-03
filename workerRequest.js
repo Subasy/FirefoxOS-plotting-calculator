b4695ebf016cc4b9299146abfe1ff7109d7171b1
Subasy <sylvie917@gmail.com>
2013-04-03 21:00:55 +0300

    Correction of worker messages.

--- workerRequest.js
+++ workerRequest.js
@@ -1,8 +1,8 @@
 var WORKER_MANAGE_REQUEST = {
-"START" : 0,
-"SET_DATA1" : 1,
-"PERFORM" : 2,
-"CLOSE" : 3
+"START" : "start",
+"SET_DATA1" : "set_data",
+"PERFORM" : "perform",
+"CLOSE" : "close"
 /* START : Initiate the worker
    SET_DATA1 : Get teh datas
    PERFORM : do the treatement with the datas
@@ -10,5 +10,5 @@ var WORKER_MANAGE_REQUEST = {
 };
 
 var WORKER_MESSAGE = {
-"CMD1" : 0
+"CMD1" : "print"
 };
